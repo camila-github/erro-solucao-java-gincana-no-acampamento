ed964c59c1980c8b2302beb839f95e1229efc4be
Camila Augusta dos Santos <camilaaugustasantos@yahoo.com.br>
2021-06-24 20:48:45 -0300

    GincanaNoAcampamento

--- java-gincana-no-acampamento.java
+++ java-gincana-no-acampamento.java
@@ -13,40 +13,39 @@ public class GinacanNoAcampamento {
 		numeroDeAlunos = Integer.parseInt(scr.next());
 		
 		while (numeroDeAlunos > 0) {
-      for (int i = 0; i < numeroDeAlunos; i++) {
+			for (int i = 0; i < numeroDeAlunos; i++) {
 				aluno = scr.next();
 				valorDaFicha = scr.next();	
 				listaDeAlunos.add(aluno.trim() + " " + valorDaFicha.trim());
-      }
-			
+			}
+				
 			indiceAtual = 0;
 			indiceAlunoEliminado = 0;
 			valorAtualFicha = Integer.parseInt(listaDeAlunos.get(indiceAtual).split(" ")[1]);
-			
+				
 			for (int i = 0; i < numeroDeAlunos - 1; i++) {
 				if (valorAtualFicha % 2 == 0) {
-          indiceAlunoEliminado = (listaDeAlunos.size() - (valorAtualFicha % listaDeAlunos.size()) + indiceAtual) % listaDeAlunos.size();
-					
+					indiceAlunoEliminado = (listaDeAlunos.size() - (valorAtualFicha % listaDeAlunos.size()) + indiceAtual) % listaDeAlunos.size();
+						
 				} else if (valorAtualFicha % 2 != 0) {
-          indiceAlunoEliminado = (valorAtualFicha % listaDeAlunos.size() + indiceAtual) % listaDeAlunos.size();
-        
-        }
-				
-			  valorAtualFicha = Integer.parseInt(listaDeAlunos.get(indiceAlunoEliminado).split(" ")[1]);
-			  listaDeAlunos.remove(indiceAlunoEliminado);
+					indiceAlunoEliminado = (valorAtualFicha % listaDeAlunos.size() + indiceAtual) % listaDeAlunos.size();
+			
+				}
+					
+				valorAtualFicha = Integer.parseInt(listaDeAlunos.get(indiceAlunoEliminado).split(" ")[1]);
+				listaDeAlunos.remove(indiceAlunoEliminado);
 
-			  indiceAtual = (valorAtualFicha % 2 == 0) ? 
+				indiceAtual = (valorAtualFicha % 2 == 0) ? 
 					((indiceAlunoEliminado <= listaDeAlunos.size() - 1) ? 
-					  indiceAlunoEliminado : 0):
-						  ((indiceAlunoEliminado == 0) ?
-							  (listaDeAlunos.size() - 1):(indiceAlunoEliminado - 1));
+						indiceAlunoEliminado : 0):
+							((indiceAlunoEliminado == 0) ?
+								(listaDeAlunos.size() - 1):(indiceAlunoEliminado - 1));
 			}
 
 			System.out.println("Vencedor(a): " + listaDeAlunos.get(0).split(" ")[0]);
-			
 			listaDeAlunos.clear();
 			numeroDeAlunos = Integer.parseInt(scr.next());
 		}
-		scr.close();
+	scr.close();
 	}
 }
